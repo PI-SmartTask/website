89aea4276c9e9e5338d2620699a4bdd9a0b96cc6
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-02-21 18:59:21 +0000

    correção de pasta

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,13 +17,13 @@ const config = {
   url: 'https://PI-SmartTask.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'PI-SmartTask', // Usually your GitHub org/user name.
   projectName: 'PI-SmartTask', // Usually your repo name.
-
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
